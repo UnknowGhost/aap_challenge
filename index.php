246169cf526cb754d1cb89e2b0bc270df3f2d853
Unknowghosty <justin.zonne@gmail.com>
2019-11-14 13:59:17 +0100

    opdracht 2

--- index.php
+++ index.php
@@ -6,3 +6,43 @@
  * Time: 10:26
  */
 
+$apen[] = "Baviaan";
+$apen[] = "Guereza";
+$apen[] = "Langoer";
+$apen[] = "Tamarin";
+$apen[] = "Neusaap";
+$apen[] = "Halfaap";
+$apen[] = "Mandril";
+$apen[] = "Oeakari";
+$apen[] = "Faunaap";
+$apen[] = "Hoelman";
+$apen[] = "Meerkat";
+$apen[] = "Oormaki";
+$apen[] = "Gorilla";
+$apen[] = "Kuifaap";
+$apen[] = "Mensaap";
+$apen[] = "Spinaap";
+
+?>
+<link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="index.css">
+
+<div>
+    <img src="top_img/monkey-business.jpg" alt="monkey-business">
+    <p class="header"> <h1>select your monkey!</h1> </p>
+    <img src="top_img/monkey_swings.png" alt="monkey_swings">
+</div>
+
+<?php
+
+    echo "<div class='names'>";
+
+    foreach ($apen as $aap) {
+
+        echo "<a href='https://www.google.nl/search?q=. $aap .&tbm=isch'> $aap <br/> </a>";
+    }
+
+
+    echo "</div>";
+
+    ?>
